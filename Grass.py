710711fa040a31d8adb416944426547fec10a86c
mariano <a1271949@uabc.edu.mx>
2023-09-29 16:53:22 -0700

    "playa"

--- Grass.py
+++ Grass.py
@@ -5,13 +5,13 @@ for x in range(10):
 	#Seperate cases so it lets us push because is different each time.
 	if(x%2)==0:
 		subprocess.run(["git","pull"])
-		subprocess.run("echo '1' | cat > nueve.txt")
-		subprocess.run("git add .")
-		subprocess.run("git commit -m 'Playa'")
-		subprocess.run("git push origin main")
+		subprocess.run(["echo","1","|","cat",">","nueve.txt"])
+		subprocess.run(["git","add","."])
+		subprocess.run(["git","commit","-m",'"playa"'])
+		subprocess.run(["git","push","origin",'main'])
 	else:
-		subprocess.run("git pull")
-		subprocess.run("echo '2' | cat > nueve.txt")
-		subprocess.run("git add .")
-		subprocess.run("git commit -m 'Sol'")
-		subprocess.run("git push origin main")
+		subprocess.run(["git","pull"])
+		subprocess.run(["echo","2","|","cat",">","nueve.txt"])
+		subprocess.run(["git","add","."])
+		subprocess.run(["git","commit","-m",'"sol"'])
+		subprocess.run(["git","push","origin",'main'])
