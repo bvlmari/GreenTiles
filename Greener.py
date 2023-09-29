bfb7e6ab65c3dcd7e0e33aa12ffd60acbceaee20
mariano <a1271949@uabc.edu.mx>
2023-09-29 16:11:40 -0700

    Playa

---
+++ Greener.py
@@ -0,0 +1,14 @@
+import os
+for x in range(10):
+	if(x%2)==0:
+		os.system("git pull")
+		os.system("echo '1' | cat > nueve.txt")
+		os.system("git add .")
+		os.system("git commit -m 'Playa'")
+		os.system("git push origin main")
+	else:
+		os.system("git pull")
+		os.system("echo '2' | cat > nueve.txt")
+		os.system("git add .")
+		os.system("git commit -m 'Sol'")
+		os.system("git push origin main")
