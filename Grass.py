e17026564b3830dcf0aac1c5af90f0aa208bf4cf
mariano <a1271949@uabc.edu.mx>
2023-09-29 18:38:22 -0700

    Playa

--- Grass.py
+++ Grass.py
@@ -1,23 +1,33 @@
 import subprocess
 import sys
 
-a = int(sys.argv[1])
+def main():
+	a = int(sys.argv[1])
 
-#iteration
-for x in range(a):
-	y = x
-	s = f"echo '{y}' | cat > nueve.txt"
+	#iteration
+	for x in range(a):
+		y = x
+		s = f"echo '{y}' | cat > nueve.txt"
 
-	#Seperate cases so it lets us push because is different each time.
-	if(x%2)==0:
-		subprocess.run("git pull", shell=True)
-		subprocess.run(s, shell=True)
-		subprocess.run("git add .", shell=True)
-		subprocess.run("git commit -m 'Playa'", shell=True)
-		subprocess.run("git push origin main", shell=True)
-	else:
-		subprocess.run("git pull", shell=True)
-		subprocess.run("echo '2' | cat > nueve.txt", shell=True)
-		subprocess.run("git add .", shell=True)
-		subprocess.run("git commit -m 'Sol'", shell=True)
-		subprocess.run("git push origin main", shell=True)
+		#Seperate cases so it lets us push because is different each time.
+		if(x%2)==0:
+			GreenScript(s)
+		else:
+			GreenScriptRev(s)
+
+def GreenScript(s):
+	subprocess.run("git pull", shell=True)
+	subprocess.run(s, shell=True)
+	subprocess.run("git add .", shell=True)
+	subprocess.run("git commit -m 'Playa'", shell=True)
+	subprocess.run("git push origin main", shell=True)
+
+def GreenScriptRev(s):
+	subprocess.run("git pull", shell=True)
+	subprocess.run(s, shell=True)
+	subprocess.run("git add .", shell=True)
+	subprocess.run("git commit -m 'Sol'", shell=True)
+	subprocess.run("git push origin main", shell=True)
+
+if __name__ == "__main__":
+	main()
