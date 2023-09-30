7cac8dc9fbfcc8ca8229edb1d501b6a76483a6af
mariano <a1271949@uabc.edu.mx>
2023-09-29 18:27:27 -0700

    Playa

--- Grass.py
+++ Grass.py
@@ -5,10 +5,13 @@ a = int(sys.argv[1])
 
 #iteration
 for x in range(a):
+	y = x
+	s = f"echo '{y}' | cat > nueve.txt"
+
 	#Seperate cases so it lets us push because is different each time.
 	if(x%2)==0:
 		subprocess.run("git pull", shell=True)
-		subprocess.run("echo '1' | cat > nueve.txt", shell=True)
+		subprocess.run(s, shell=True)
 		subprocess.run("git add .", shell=True)
 		subprocess.run("git commit -m 'Playa'", shell=True)
 		subprocess.run("git push origin main", shell=True)
